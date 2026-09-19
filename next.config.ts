43557bff056e9ef784e3ffb67bcb4ff7042159b6
Mridul Rajgaria <iammridul222@gmail.com>
2026-09-19 18:24:01 +0530

    chore: setup github pages static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/portfolio-2026",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
